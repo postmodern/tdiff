24b073a9108f3df2fe8acbb98950820690bcc0c2
Postmodern <postmodern.mod3@gmail.com>
2010-11-13 18:41:44 -0800

    Added a spec for determining changes in order.

--- spec/tdiff_spec.rb
+++ spec/tdiff_spec.rb
@@ -34,6 +34,16 @@ describe TDiff do
         Node.new('subleaf1', [])
       ])
     ])
+
+    @changed_order = Node.new('root', [
+      Node.new('leaf2', [
+        Node.new('subleaf1', [])
+      ]),
+
+      Node.new('leaf1', [
+        Node.new('subleaf1', [])
+      ])
+    ])
   end
 
   it "should tell if two trees are identical" do
@@ -66,5 +76,22 @@ describe TDiff do
     changes.length.should == 1
     changes[0][0].should == '-'
     changes[0][1].should == @tree.children[0].children[0]
+  end
+
+  it "should detect when the order of children has changed" do
+    changes = @tree.tdiff(@changed_order).to_a
+
+    changes.length.should == 4
+    changes[0][0].should == '-'
+    changes[0][1].should == @tree.children[0]
+
+    changes[1][0].should == '+'
+    changes[1][1].should == @changed_order.children[0]
+
+    changes[2][0].should == '-'
+    changes[2][1].should == @tree.children[1]
+
+    changes[3][0].should == '+'
+    changes[3][1].should == @changed_order.children[1]
   end
 end
